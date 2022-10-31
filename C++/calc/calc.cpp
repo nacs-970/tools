d18d9d48ec0f070d6392c0857e793c03f890ab17
nacs-970 <atitape6958@gmail.com>
2022-10-31 17:39:51 +0700

    10-31-2022

---
+++ C++/calc/calc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	int a,b,c;
+	cout<<"Enter first number\n";
+	cin>>a;
+	cout<<"Enter sec number\n";
+	cin>>b;
+	c=a+b;
+	cout<<"Sum : "<< c << endl;
+	return 0;
+}
