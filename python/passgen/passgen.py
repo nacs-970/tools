c43ad0497ec453ea32061fe1339b0d0925108bff
nacs-970 <atitape6958@gmail.com>
2023-03-26 21:36:51 +0700

    03/26/23-09:36:51 PM

--- python/passgen/passgen.py
+++ python/passgen/passgen.py
@@ -1,4 +1,5 @@
 import random
+from sys import argv
 
 upper = "ABCDEFGHIJKLMNOPQRSTUVVWXYZ"
 lower = upper.lower()
@@ -18,10 +19,14 @@ if d:
 if p:
     var += punc
 
-length = int(input("Enter Length : "))
-amount = int(input("Enter Amount : "))
-
-for x in range(amount):
-    password = "".join(random.sample(var,length))
+if len(argv) >= 2:
+    password = "".join(random.sample(var,int(argv[1])))
     print(password)
+
+else:
+    length = int(input("Enter Length : "))
+    amount = int(input("Enter Amount : "))
+    for x in range(amount):
+        password = "".join(random.sample(var,length))
+        print(password)
 
